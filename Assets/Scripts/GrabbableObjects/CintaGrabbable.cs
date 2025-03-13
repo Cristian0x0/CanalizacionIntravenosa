62d3582552b24a47b9412ca4e7cb3514d16953d1
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-03-13 14:54:46 +0100

    Mas cosas del esparadrapo

---
+++ Assets/Scripts/GrabbableObjects/CintaGrabbable.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Oculus.Interaction;
+using System;
+using Meta.XR.MRUtilityKit;
+
+public class CintaGrabbable : MonoBehaviour
+{
+    
+    public GameObject TrozoEsparatrapo;
+
+    private Grabbable grabbable;
+    public Rigidbody rb;
+    private bool SoloUnUso = false;
+    private MeshRenderer meshRenderer;
+    private Transform Spawn;
+
+    void Awake()
+    {
+        grabbable = GetComponent<Grabbable>();
+        grabbable.Agarrado = false;
+        meshRenderer = transform.Find("Visuals/EsparadrapoMesh/Mesh")?.GetComponent<MeshRenderer>();
+        
+        Debug.Log("Estoy siendo creado");
+    }
+
+    private void Start()
+    {
+        Spawn = transform.parent?.Find("Spawn");
+    }
+
+    void Update()
+    {
+        if (grabbable.Agarrado && !SoloUnUso)
+        {
+            SoloUnUso = true;
+            meshRenderer.enabled = true;
+            GameObject nuevoTrozo = Instantiate(TrozoEsparatrapo, Spawn.position, Quaternion.identity);
+            nuevoTrozo.transform.SetParent(transform.parent);
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
