733e7d9da05b8fdc3c64d7f2bdf9a31183356059
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-03-17 10:31:16 +0100

    SistemaSuero

---
+++ Assets/Scripts/ColisionSistemaSuero.cs
@@ -0,0 +1,50 @@
+using Oculus.Interaction;
+using UnityEngine;
+
+public class ColisionSistemaSuero : MonoBehaviour
+{
+    public Transform puntoColocacion; // Lista de puntos de colocación
+    private GameObject sistema;
+    private Rigidbody rb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SistemaSuero"))
+        {
+            sistema = other.gameObject; // Guardamos referencia al bote
+            rb = sistema.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.LockKinematic();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("SistemaSuero") && sistema == other.gameObject)
+        {
+            if (rb != null)
+            {
+                rb.UnlockKinematic();
+            }
+            sistema = null; // Si el bote sale, lo olvidamos
+        }
+    }
+
+    public void SoltarBote()
+    {
+        if (sistema != null)
+        {
+            sistema.GetComponent<SistemaGrabbable>().enabled = false;
+
+
+            sistema.transform.parent = puntoColocacion;
+            sistema.transform.localPosition = Vector3.zero;
+
+            sistema.transform.rotation = puntoColocacion.rotation;
+
+            sistema.GetComponent<SistemaGrabbable>().enabled = true;
+        }
+    }
+}
