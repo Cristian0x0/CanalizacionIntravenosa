9c5f2b6a374d33e0f5e2bd4b22d9db59b8d46bed
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-03-28 17:13:52 +0100

    Iluminacion

--- Assets/Scripts/FlujoDelJuego/GameManager.cs
+++ Assets/Scripts/FlujoDelJuego/GameManager.cs
@@ -36,58 +36,76 @@ public class GameManager : MonoBehaviour
         {
             case GameState.BuscarObjetos:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[0]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[0]);
                 break;
             case GameState.PrepararSistema:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[1]);
-                
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[1]);
+
                 break;
             case GameState.ColocarCompresor:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[2]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[2]);
                 break;
             case GameState.PalparVena:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[3]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[3]);
                 break;
             case GameState.AplicarAntiseptico:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[4]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[4]);
                 break;
             case GameState.PonerseGuantes:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[5]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[5]);
                 break;
             case GameState.DesenfundarCateter:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[6]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[6]);
                 break;
             case GameState.FijarPiel:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[7]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[7]);
                 break;
             case GameState.InsertarCateter:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[8]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[8]);
                 break;
             case GameState.ExtraerAguja:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[9]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[9]);
                 break;
             case GameState.RetirarCompresor:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[10]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[10]);
                 break;
             case GameState.AbrirLlave:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[11]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[11]);
                 break;
             case GameState.LimpiarZona:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[12]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[12]);
                 break;
             case GameState.ColocarAposito:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[13]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[13]);
                 break;
             case GameState.FijarSistema:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[14]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[14]);
                 break;
             case GameState.DesecharAguja:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[15]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[15]);
                 break;
             case GameState.RecogerMaterial:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[16]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[16]);
                 break;
             case GameState.RetirarGuantes:
                 LEDPantallas.SetTexture("_BaseMap", Instrucciones[17]);
+                LEDPantallas.SetTexture("_EmissionMap", Instrucciones[17]);
                 break;
         }
 
