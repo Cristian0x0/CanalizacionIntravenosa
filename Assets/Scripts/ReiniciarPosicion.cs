b8fca58f617ddde18428cfb5cc5ac4b0c3e1886a
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-04-02 12:37:39 +0200

    ReinicioPosicionObjetos

--- Assets/Scripts/ReiniciarPosicion.cs
+++ Assets/Scripts/ReiniciarPosicion.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 public class ReiniciarPosicion : MonoBehaviour
 {
 
-    private Transform posicionInicial;
+    private Vector3 posicionInicial;
+    private Quaternion rotacionInicial;
+
+    private Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        posicionInicial = this.transform;
+        rb = GetComponent<Rigidbody>();
+
+        posicionInicial = transform.position;
+        rotacionInicial = transform.rotation;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Suelo")
         {
-            this.transform.position = posicionInicial.position;
-            this.transform.rotation = posicionInicial.rotation;
+            transform.position = posicionInicial;
+            transform.rotation = rotacionInicial;
+
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
         }
     }
 }
