e585e7cd99ff3cece15b21051ae3490971e91f81
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-04-04 12:22:21 +0200

    MasMejoras

--- Assets/Scripts/ReiniciarPosicion.cs
+++ Assets/Scripts/ReiniciarPosicion.cs
@@ -6,7 +6,13 @@ public class ReiniciarPosicion : MonoBehaviour
     private Vector3 posicionInicial;
     private Quaternion rotacionInicial;
 
+    [Tooltip("Opcional, para objetos conectados a otros, como es el caso del sistema de suero.")]
+    [SerializeField] private Transform segundoObjetoOpcional;
+    private Vector3 segundaPosicionInicial;
+    private Quaternion segundaRotacionInicial;
+
     private Rigidbody rb;
+    private Rigidbody rbOpcional;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +21,14 @@ public class ReiniciarPosicion : MonoBehaviour
 
         posicionInicial = transform.position;
         rotacionInicial = transform.rotation;
+
+        if (segundoObjetoOpcional != null)
+        {
+            segundaPosicionInicial = segundoObjetoOpcional.position;
+            segundaRotacionInicial = segundoObjetoOpcional.rotation;
+            rbOpcional = segundoObjetoOpcional.GetComponent<Rigidbody>();
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,6 +42,19 @@ public class ReiniciarPosicion : MonoBehaviour
             {
                 rb.linearVelocity = Vector3.zero;
                 rb.angularVelocity = Vector3.zero;
+            }
+
+            if (segundoObjetoOpcional!= null) {
+
+                segundoObjetoOpcional.position = segundaPosicionInicial;
+                segundoObjetoOpcional.rotation = segundaRotacionInicial;
+
+                if (rbOpcional != null)
+                {
+                    Debug.Log("Reiniciando Rigidbody opcional");
+                    rbOpcional.linearVelocity = Vector3.zero;
+                    rbOpcional.angularVelocity = Vector3.zero;
+                }
             }
         }
     }
