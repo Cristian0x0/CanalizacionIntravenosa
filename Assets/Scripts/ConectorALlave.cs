7090702747bfdb914310c9cc7f62aba6fe699749
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-04-09 09:24:32 +0200

    ConectorModificadoNoFuncional

---
+++ Assets/Scripts/ConectorALlave.cs
@@ -0,0 +1,70 @@
+using Oculus.Interaction;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class ConectorALlave : MonoBehaviour
+{
+    [SerializeField] private Transform NuevaPosicion;
+    [SerializeField] private GameObject Padre;
+    [SerializeField] private GameObject colliders; //Estos hay que quitarlos para que el conector del sistema de suero no pueda ser cogido
+    [SerializeField] private SoltarConectorSistema soltarConectorSistema;
+    private SoltarConectorSistema soltarConectorSistema2;
+    private Grabbable grabbable;
+    private ReiniciarPosicion reiniciarPosicion;
+    private Rigidbody rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Llave3Pasos"))
+        {
+            rb.LockKinematic();
+            grabbable = other.GetComponent<Grabbable>();
+            if (grabbable != null)
+            {
+                grabbable.enabled = false; // Desactivar el Grabbable
+                StartCoroutine(EsperarUnSegundo());
+            }
+            
+            other.transform.SetParent(Padre.transform);
+            other.transform.position = NuevaPosicion.position;
+            other.transform.rotation = NuevaPosicion.rotation;
+            transform.SetParent(other.transform);
+
+            // Desactivar el script ReiniciarPosicion y activar el limite del cable
+            reiniciarPosicion = other.GetComponent<ReiniciarPosicion>();
+            if (reiniciarPosicion != null)
+            {
+                reiniciarPosicion.enabled = false;
+            }
+            if (soltarConectorSistema != null)
+            {
+                soltarConectorSistema.enabled = true;
+            }
+            soltarConectorSistema2 = other.GetComponent<SoltarConectorSistema>();
+            if (soltarConectorSistema2 != null)
+            {
+                soltarConectorSistema2.enabled = true;
+            }
+
+
+            colliders.SetActive(false);
+        }
+    }
+
+    IEnumerator EsperarUnSegundo()
+    {
+        // Esperamos 1 segundo
+        yield return new WaitForSeconds(1f);
+
+        // Después de 1 segundo, se ejecuta este código
+        grabbable.enabled = true;
+    }
+}
+
