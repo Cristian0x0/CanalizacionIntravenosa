cc6c11a1d80a71a1170b43f8d86344642ff82f38
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-04-11 10:15:15 +0200

    PrimeraVersionPalparVena

---
+++ Assets/Scripts/PalparVena/NearbyArea.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Oculus.Haptics;
+public class NearbyArea : MonoBehaviour
+{
+    private List<Transform> handsInside = new List<Transform>();
+    public bool IsHandInside => handsInside.Count > 0;
+
+    public HapticSource hapticSource; // Asigna desde el Inspector
+    public float maxDistance = 0.5f;
+
+    private bool hapticPlaying = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hands") && !handsInside.Contains(other.transform))
+        {
+            handsInside.Add(other.transform);
+
+            if (!hapticPlaying && hapticSource != null)
+            {
+                hapticSource.Play();
+                hapticPlaying = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hands"))
+        {
+            handsInside.Remove(other.transform);
+
+            if (handsInside.Count == 0 && hapticSource != null)
+            {
+                hapticSource.Stop();
+                hapticPlaying = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (IsHandInside && hapticSource != null)
+        {
+            float closestDistance = float.MaxValue;
+
+            foreach (var hand in handsInside)
+            {
+                float distance = Vector3.Distance(transform.position, hand.position);
+                if (distance < closestDistance)
+                    closestDistance = distance;
+            }
+
+            float normalized = Mathf.Clamp01(1 - (closestDistance / maxDistance));
+            normalized = Mathf.Pow(normalized, 2f);
+            hapticSource.amplitude = normalized;
+        }
+    }
+}
