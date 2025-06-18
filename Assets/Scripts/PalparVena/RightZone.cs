74752753f3214c4f08d8cc7f19638cb343cc45cc
Cristian Hernández Delgado <145202121+Cristian0x0@users.noreply.github.com>
2025-06-18 14:08:40 +0200

    ErrorReinicioNivelSolucionado
    La pestaña Achievements no se debe ocultar en la jerarquia

--- Assets/Scripts/PalparVena/RightZone.cs
+++ Assets/Scripts/PalparVena/RightZone.cs
@@ -50,6 +50,7 @@ public class RightZone : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log($"Contact Count: {contactCount}");
         if (timerActive)
         {
             stayTimer += Time.deltaTime;
